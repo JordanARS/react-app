72ca3b66d54efdd01d17ff877e6e5d7d7896773b
JordanARS <jordanalejandrorodriguez@gmail.com>
2023-04-19 23:06:24 -0500

    Solo IMG de Universidad

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ function App() {
 
   return (
   
-       /* <div className="flex w-full h-screen">
+        /* <div className="flex w-full h-screen">
         <div className="w-full flex items-center justify-center lg:w-1/2">
           <Form />
         </div>       
-          <div className="hidden relative lg:flex w-1/2 h-full items-center justify-center bg-gray-200">
-          <div className="w-60 h-60 bg-gradient-to-tr from-sky-300 to-indigo-300 rounded-full animate-spin"/>
-          <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"/>
+          <div className="hidden relative lg:flex w-1/2 h-full items-center justify-center bg-[#F7F7F7]">
+          <img src="/LogoUCC.png"></img>
         </div>
-      </div> */ 
- 
-        <Dashboard />
+      </div>  */ 
+
+        <Dashboard /> 
         
   );
 }
