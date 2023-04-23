212e06b401dd7ab0ce6d78469ec885ce666757ac
JordanARS <jordanalejandrorodriguez@gmail.com>
2023-04-23 17:15:40 -0500

    Login Modificado

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
   
         <div className="flex w-full h-screen">
-        <div className="w-full flex items-center justify-center lg:w-1/2">
+        <div className="bg-[#111827] w-full flex items-center justify-center lg:w-1/2">
           <Form />
         </div>       
           <div className="hidden relative lg:flex w-1/2 h-full items-center justify-center bg-[#F7F7F7]">
-          <img src="/LogoUCC.png"></img>
+          <img src="/FondoNegro.webp" className="h-screen"></img>
         </div>
       </div>  
 
--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -5,29 +5,29 @@ import Sesion from '../auth/Sesion'
 export default function Form(){
     return(
         
-        <form className='bg-white px-10 py-20 rounded-e-3xl border-2 border-gray-400'>
+        <form className='px-10 py-20 rounded-e-3xl'>
             
-            <h1 className='text-5xl font-semibold'>Bienvenido</h1>
-            <p className='font-medium text-lg text-gray-700 mt-4'>
+            <h1 className='text-5xl text-white font-semibold'>Bienvenido</h1>
+            <p className='font-medium text-lg text-gray-200 mt-4'>
                 ¡Bienvenido! Por favor ingrese sus datos</p>
 
             <div className='mt-5'>
                 <div>
-                    <label className='text-lg font-medium'>Correo</label>
-                    <input className='w-full border-2 border-gray-200 rounded-xl p-3 mt-1 bg-slate-50' placeholder='Ingrese su correo'/>
+                    <label className='text-lg text-white font-medium'>Correo</label>
+                    <input className='w-full border border-gray-500 rounded-xl p-3 mt-1 bg-[#1D2432]' placeholder='Ingrese su correo'/>
                 </div>
                 
-                <div>
-                    <label className='text-lg font-medium'>Contraseña</label>
-                    <input className='w-full border-2 border-gray-200 rounded-xl p-3 mt-1 bg-slate-50' placeholder='Ingrese su contraseña' type='password'/>
+                <div className="mt-4">
+                    <label className='text-lg text-white font-medium'>Contraseña</label>
+                    <input className='w-full border border-gray-500 rounded-xl p-3 mt-1 bg-[#1D2432]' placeholder='Ingrese su contraseña' type='password'/>
                 </div>
                 <div className='mt-8 flex justify-center items-center'>
                     
-                    <button className='font-medium text-sky-500 ' type="">¿Ha olvidado su contraseña?</button>
+                    <button className='font-medium text-sky-300 ' type="">¿Ha olvidado su contraseña?</button>
                 </div>
                 <div className='mt-6 flex flex-col gap-y-4'>
                     
-                    <Link className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 bg-indigo-200 font-semibold text-lg text-center rounded-xl' to={"/contenido"}>Ingresar</Link>
+                    <Link className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 bg-indigo-300 font-semibold text-lg text-center rounded-xl' to={"/contenido"}>Ingresar</Link>
                     
 
                 </div>
